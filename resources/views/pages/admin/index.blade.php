e6b85f7da5ae02c2c051057c316b32c410b1793a
Nathan Ari Soetopo <nathanarisoetopo@Nathans-MacBook-Pro.local>
2024-05-12 19:39:51 +0700

    init:scanner

--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -4,5 +4,24 @@
 @section('sub_title', 'Dashboard')
 
 @section('main')
-    <p>Hello World</p>
+<p>TEST</p>
+    <div id="scanner-container"></div>
+@endsection
+
+@section('script')
+    <script>
+        const scanner = new Scanner({
+            // Specify the element where the scanner will be rendered
+            video: document.getElementById('scanner-container'),
+
+            // Callback function to handle the scanned content
+            onScan: content => {
+                // Display the scanned content in the specified element
+                document.getElementById('scanned-content').innerText = content;
+            }
+        });
+
+        // Start the scanner
+        scanner.start();
+    </script>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,42 @@
 @section('sub_title', 'Dashboard')
 
 @section('main')
-    <p>Hello World</p>
+    <div class="flex flex-col justify-center items-center">
+        <h1>Scan QR Codes</h1>
+        <div class="section flex justify-center w-full">
+            <div id="my-qr-reader"></div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script src="https://unpkg.com/html5-qrcode"></script>
+
+    <script>
+        function domReady(fn) {
+            if (
+                document.readyState === "complete" || document.readyState === "interactive"
+            ) {
+                setTimeout(fn, 1000);
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
+        }
+
+        domReady(function() {
+
+            function onScanSuccess(decodeText, decodeResult) {
+                alert("You Qr is : " + decodeText, decodeResult);
+            }
+
+            let htmlscanner = new Html5QrcodeScanner(
+                "my-qr-reader", {
+                    fps: 10,
+                    qrbos: 250,
+                    disableFlip: false
+                }
+            );
+            htmlscanner.render(onScanSuccess);
+        });
+    </script>
 @endsection
