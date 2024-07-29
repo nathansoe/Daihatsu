9e0c488262f164ac5610ed139480c9038eff0b33
development-neo <110153881+development-neo@users.noreply.github.com>
2024-07-29 21:09:32 +0700

    Update barcode.blade.php

--- resources/views/pages/guest/barcode.blade.php
+++ resources/views/pages/guest/barcode.blade.php
@@ -8,7 +8,7 @@
     <title>Print Barcode</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
-        @media print {
+/*         @media print {
             @page {
                 size: A4;
                 margin: 0;
@@ -19,12 +19,26 @@
             .print-hide{
                 display: none
             }
+        } */
+        @media print {
+            img.barcode {
+                width: 600px;
+                height: auto;
+            }
+            #barcodeResultRegister {
+                width: 300px;
+                height: auto;
+                left: 25%;
+            }
+            #printBtn {
+                display: none;
+            }
         }
     </style>
 </head>
 
 <body>
-    <div class="w-full flex justify-center my-4 print-hide">
+<!--     <div class="w-full flex justify-center my-4 print-hide">
         <button onclick="window.print()" type="button"
             class="text-white text-md bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-4 py-2.5 me-3 mb-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Print
             / Save</button>
@@ -35,6 +49,16 @@
             <img src="#" id="barcodeResultRegister" style="position: absolute; top:40%; left: 28%"
                 class="h-[12vh] max-w-full" alt="result">
         </div>
+    </div> -->
+    <div class="w-full h-full flex justify-center items-center">
+        <div id="registerPrintBarcode" style="position: relative;">
+            <img class="barcode w-full h-full object-cover" src="{{ asset('img/barcode_parent.png') }}" alt="barcode">
+            <img src="#" id="barcodeResultRegister" class="h-[130px] md:h-[360px] lg:h-[480px] max-w-full absolute top-[36%] left-[28%] lg:left-[34%]" alt="result">
+        </div>
+    </div>
+
+    <div id="printBtn" class="w-full flex justify-center my-4">
+        <button onclick="window.print()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Print / Save</button>
     </div>
 </body>
 
